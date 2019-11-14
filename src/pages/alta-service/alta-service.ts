e569b8aafb9c34b590e0306eeada70da81fa0214
ely2018 <42919699+ely2018@users.noreply.github.com>
2019-11-14 01:59:41 -0300

    validacion campos publicar herramienta

--- src/pages/alta-service/alta-service.ts
+++ src/pages/alta-service/alta-service.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams , ToastController} from 'ionic-angular';
 
 /**
  * Generated class for the AltaServicePage page.
@@ -14,12 +14,65 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'alta-service.html',
 })
 export class AltaServicePage {
+  name: string='';
+  cant: string='';
+  file: string='';
+  ctria: string='';
+    /**'Albañileria',
+    'Electricidad',
+    'Industriales',
+    'Cortar',
+    'Sujecion',
+    'Jardineria y Paisajismo'
+  ];*/
+  descripcion='';
+  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AltaServicePage');
-  }
 
+publicar(){
+  if(this.name==""){
+    const toast =this.toastCtrl.create({
+      message:"Nombre es requerido",
+      duration:3000
+    });
+    toast.present();
+  }else  if(this.cant==""){
+    const toast =this.toastCtrl.create({
+      message:"Cantidad es requerida",
+      duration:3000
+  });
+  toast.present();
+  }else  if(this.file==""){
+    const toast =this.toastCtrl.create({
+      message:"Imagen es requerida",
+      duration:3000
+    });
+    toast.present();
+  }else  if(this.ctria==""){
+    const toast =this.toastCtrl.create({
+      message:"Categoria es requerida",
+      duration:3000
+    });
+    toast.present();
+  }else  if(this.descripcion==""){
+      const toast =this.toastCtrl.create({
+        message:"Descripcion es requerida",
+        duration:3000
+      });
+      toast.present();
+
+  }else{
+    const toast =this.toastCtrl.create({
+      message:"Publicacion exitosa",
+      duration:3000
+    });
+    toast.present();
+  }
+    console.log('Enviado')
+    //proses if the password and username have been fillend in.
+    
+  }
 }
